42d15122514efdfbed4da2eb0693411cd6af8617
Dog-Broad <rushyendra.7004@gmail.com>
2022-04-20 04:28:37 +0530

    Matrix multiplication fixes to syntax of wk7a

--- WEEK7B.C
+++ WEEK7B.C
@@ -1,18 +1,18 @@
-//Matrix Addition
+//Matrix Multiplication
 
 #include<stdio.h>
 #include<conio.h>
 
 void main()
 {
-    int i, j, r1, c1, r2, c2, a[10][10], b[10][10], c[10][10];
+    int i, j, k, r1, c1, r2, c2, a[10][10], b[10][10], c[10][10];
 
     printf("Row and Column size of 1st matrix: ");
     scanf("%d%d", r1, c1);
     printf("Row and Column size of 2nd matrix: ");
     scanf("%d%d", r2, c2);
 
-    if((r1==r2) && (c1==c2))
+    if(r1==c2)
     {
         //input
         printf("Elements of 1st Matrix: ");
@@ -27,13 +27,14 @@ void main()
             for(j=0; j<c2; j++)
                 scanf("%d", b[i][j]);
         }
-
+        
         //processing
-        for(i=0; i<r1; i++)
+        for(i=0; i<r2; i++)
         {
             for(j=0; j<c1; j++)
             {
-                c[i][j] = a[i][j] + b[i][j];
+                for(k=0; i<c1; k++)
+                    c[i][j] = a[i][k] * b[k][j];
             }
         }
 
